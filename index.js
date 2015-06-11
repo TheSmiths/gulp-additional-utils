89322e62abe412f0ad85825a601166f60e7d7612
ktorz <matthias.benkort@gmail.com>
2015-06-11 20:14:38 +0200

    Change regex for pid killing

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var exec = require('child_process').exec;
 
 module.exports = {
     clean_env: function (callback) {
-        exec('ps', function (err, stdout) {
+        exec('ps -e -o pid,command | grep node', function (err, stdout) {
             if (err) { 
                 process.stdout.write('\033[1;30mFailed to clean environment\033[0m\n');
             } else {
-                var re = /([0-9]+).+node.+(tishadow\sserver|ti)\s.+/g, 
+                var re = /([0-9]+)[^\n]+(--appify|tishadow)[^\n]+\n/g,
                     pid;
 
                 while ((pid = re.exec(stdout)) !== null) {
